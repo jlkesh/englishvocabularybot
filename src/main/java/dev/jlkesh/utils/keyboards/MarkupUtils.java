94f65a659719758dc75a7bc500ba05d20fb342fb
jlkesh <john.lgd65@gmail.com>
2022-12-22 18:09:57 +0500

    initial

--- src/main/java/dev/jlkesh/utils/keyboards/MarkupUtils.java
+++ src/main/java/dev/jlkesh/utils/keyboards/MarkupUtils.java
@@ -1,6 +1,7 @@
 package dev.jlkesh.utils.keyboards;
 
 import dev.jlkesh.utils.Command;
+import dev.jlkesh.utils.MessageUtils;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
@@ -10,12 +11,12 @@ import java.util.Collections;
 
 public class MarkupUtils {
 
-    public static ReplyKeyboard mainMenu() {
+    public static ReplyKeyboard mainMenu(String language) {
         var board = new ReplyKeyboardMarkup();
         board.setResizeKeyboard(true);
         var row = new KeyboardRow();
-        var button1 = new KeyboardButton(Command.ADD_WORD);
-        var button2 = new KeyboardButton(Command.START_DAILY_QUIZ);
+        var button1 = new KeyboardButton(MessageUtils.getTranslatedText("add.new.word", language));
+        var button2 = new KeyboardButton(MessageUtils.getTranslatedText("start.daily.quiz", language));
         row.add(button1);
         row.add(button2);
         board.setKeyboard(Collections.singletonList(row));
